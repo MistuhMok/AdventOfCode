fff932e1827894813fb2ba45b3e54bd3d966c93b
Alex Mok <mokalexander00@gmail.com>
2019-11-18 18:04:04 -0500

    feat: Completed both parts

--- 2018/Day 1/Day 1.js
+++ 2018/Day 1/Day 1.js
@@ -1,8 +1,32 @@
-var fs = require('fs');
-var array = fs
+const fs = require('fs');
+const array = fs
   .readFileSync('PuzzleInput.txt')
   .toString()
   .split('\r\n')
   .map(Number);
 
-console.log(array);
+// console.log(array);
+
+//Part 1 - Sum all frequencies
+//Returns 474
+// console.log(array.reduce((acc, curr) => acc + curr, 0));
+
+//Part 2 - Find number frequency reached twice
+//Returns 137041
+function findDuplicate() {
+  const hash = {};
+  let sum = 0;
+  let found = false;
+
+  while (!found) {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+      if (hash[sum]) {
+        console.log(sum);
+        return;
+      } else hash[sum] = true;
+    }
+  }
+}
+
+findDuplicate();
