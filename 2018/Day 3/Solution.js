738fc907af7bf449002093352c518203fbf45c22
Alex Mok <mokalexander00@gmail.com>
2019-11-19 17:27:55 -0500

    feat: Solved part 1

--- 2018/Day 3/Solution.js
+++ 2018/Day 3/Solution.js
@@ -5,6 +5,8 @@ const array = fs
   .split('\n')
   .map(ele => ele.slice(ele.indexOf('@') + 2));
 
+//Split input and dimensions into separate arrays
+//Not necessary but easier to read
 const width = [];
 const height = [];
 array.forEach(ele =>
@@ -20,3 +22,49 @@ array.forEach(ele =>
     +ele.slice(ele.indexOf('x') + 1),
   ])
 );
+
+// const width = [
+//   [1, 4],
+//   [3, 4],
+//   [5, 2],
+// ];
+// const height = [
+//   [3, 4],
+//   [1, 4],
+//   [5, 2],
+// ];
+
+//Part 1 - Figure out area of overlapping parts
+//Returns 97218
+function overlap() {
+  const board = [];
+  for (let i = 0; i < 1000; i++) {
+    board.push(new Array(1000).fill(0));
+  }
+
+  let pointer = 0;
+  let count = 0;
+
+  while (pointer < height.length) {
+    //Row and col are tuples [starting point, range]
+    let row = height[pointer];
+    let col = width[pointer];
+
+    //Count each square if something already exists there
+    //Make sure not to double count repeat overlapped squares
+    for (let i = 0; i < row[1]; i++) {
+      for (let j = 0; j < col[1]; j++) {
+        if (board[row[0] + i][col[0] + j] === 1) {
+          count++;
+          board[row[0] + i][col[0] + j] = -1;
+        } else if (board[row[0] + i][col[0] + j] === -1) continue;
+        else board[row[0] + i][col[0] + j]++;
+      }
+    }
+
+    pointer++;
+  }
+  console.log(count);
+}
+
+overlap();
