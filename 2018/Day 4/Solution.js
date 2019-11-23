7a15e177c3cf193eb14be029442fde1d8507df11
Alex Mok <mokalexander00@gmail.com>
2019-11-23 17:53:03 -0500

    completed part 1

--- 2018/Day 4/Solution.js
+++ 2018/Day 4/Solution.js
@@ -6,31 +6,30 @@ const array = fs
   .split('\r\n');
 // .split('\n');
 
-const shifts = createGuardShifts();
-// console.log(shifts, 'SHIFTS');
+const monthHash = {
+  '01': 31,
+  '02': 28,
+  '03': 31,
+  '04': 30,
+  '05': 31,
+  '06': 30,
+  '07': 31,
+  '08': 31,
+  '09': 30,
+  '10': 31,
+  '11': 30,
+  '12': 31,
+};
 
-// console.log(array);
+const shifts = createGuardShifts();
+const sleepData = organizeData();
+
 //Part 1 - Find guard with most minutes asleep and most common minute asleep
 //Not all guards fall asleep
 //Some guards fall asleep multiple times throughout a shift
 
 //Create hash of date to guard on duty for that day
 function createGuardShifts() {
-  const monthHash = {
-    '01': 31,
-    '02': 28,
-    '03': 31,
-    '04': 30,
-    '05': 31,
-    '06': 30,
-    '07': 31,
-    '08': 31,
-    '09': 30,
-    '10': 31,
-    '11': 30,
-    '12': 31,
-  };
-
   const shifts = {};
 
   for (let i = 0; i < array.length; i++) {
@@ -44,19 +43,15 @@ function createGuardShifts() {
         let day = (+key.slice(-2) + 1).toString();
         let month = key.slice(0, 2);
 
-        console.log(monthHash[month], day);
         if (monthHash[month] < day) {
-          day = 01;
           let newMonth = (+month + 1).toString();
           // console.log(newMonth);
           if (newMonth.length === 1) newMonth = '0' + newMonth;
 
-          console.log(key, 'before  ');
-          key.replace(month, newMonth);
-          console.log(key, 'after ');
-        }
-
-        key = array[i].slice(dateIndex, dateIndex + 5 - day.length) + day;
+          key = key.replace(month, newMonth);
+          key = key.slice(0, 3) + '01';
+        } else
+          key = array[i].slice(dateIndex, dateIndex + 5 - day.length) + day;
       }
 
       const idIndex = array[i].indexOf('#') + 1;
@@ -65,7 +60,6 @@ function createGuardShifts() {
       const idFinish = array[i].indexOf(' ', idIndex);
       const value = array[i].slice(idIndex, idFinish);
 
-      console.log(key);
       shifts[key] = value;
     }
   }
@@ -73,20 +67,28 @@ function createGuardShifts() {
   return shifts;
 }
 
-//Organize guard data
+//Organize guard sleep data into wake up and sleep arrays
 function organizeData() {
   const data = {};
 
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length - 1; i++) {
     const dateIndex = array[i].indexOf('-') + 1;
     let date = array[i].slice(dateIndex, dateIndex + 5);
 
     const hourIndex = array[i].indexOf(':') - 2;
-    const dayBefore = array[i].slice(hourIndex, hourIndex + 2) === '23';
-    if (dayBefore) {
-      let newDate = (+date.slice(-2) + 1).toString();
-      date =
-        array[i].slice(dateIndex, dateIndex + 5 - newDate.length) + newDate;
+    let dayBefore = false;
+    if (array[i].slice(hourIndex, hourIndex + 2) === '23') {
+      dayBefore = true;
+      let day = (+date.slice(-2) + 1).toString();
+      let month = date.slice(0, 2);
+
+      if (monthHash[month] < day) {
+        let newMonth = (+month + 1).toString();
+        if (newMonth.length === 1) newMonth = '0' + newMonth;
+
+        date = date.replace(month, newMonth);
+        date = date.slice(0, 3) + '01';
+      } else date = array[i].slice(dateIndex, dateIndex + 5 - day.length) + day;
     }
 
     if (data[date] === undefined) data[date] = [[], []];
@@ -94,11 +96,65 @@ function organizeData() {
     //14 is the index of the timestamp
     let minute = array[i].slice(15, 17);
     if (dayBefore) minute = '00';
-    if (array[i].includes('falls asleep')) data[date][0].push(+minute);
+    if (array[i].includes('falls asleep')) data[date][0].push(minute);
     else if (array[i].includes('wakes up')) data[date][1].push(minute);
   }
 
-  console.log(data);
+  return data;
 }
 
-// organizeData();
+function calculate() {
+  const guards = {};
+  let max = 0;
+  let guardId;
+
+  for (let key in sleepData) {
+    const guard = shifts[key];
+    const fallAsleep = sleepData[key][0];
+    const wakesUp = sleepData[key][1];
+    let mins = 0;
+
+    if (fallAsleep.length === 0) continue;
+    if (fallAsleep.length === 1) mins = +wakesUp[0] - +fallAsleep[0];
+    else {
+      fallAsleep.sort((a, b) => +a - +b);
+      wakesUp.sort((a, b) => +a - +b);
+
+      for (let i = 0; i < fallAsleep.length; i++) {
+        mins += +wakesUp[i] - +fallAsleep[i];
+      }
+    }
+
+    if (guards[guard] === undefined) guards[guard] = mins;
+    else guards[guard] += mins;
+
+    if (guards[guard] > max) {
+      guardId = guard;
+      max = guards[guard];
+    }
+  }
+
+  //#1571 most minutes slept
+  const guardSleepData = new Array(60).fill(0);
+
+  for (let date in shifts) {
+    if (shifts[date] === guardId) {
+      const fallAsleep = sleepData[date][0];
+      const wakesUp = sleepData[date][1];
+      fallAsleep.sort((a, b) => +a - +b);
+      wakesUp.sort((a, b) => +a - +b);
+
+      for (let i = 0; i < fallAsleep.length; i++) {
+        let start = fallAsleep[i];
+        let end = wakesUp[i];
+        while (start < end) guardSleepData[start++]++;
+      }
+    }
+  }
+
+  //54th min most commonly asleep
+  //1571 * 54 = 84834
+  console.log(guardId * guardSleepData.indexOf(Math.max(...guardSleepData)));
+}
+
+calculate();
