06f5722c3ede4975a9392be0d9768839d60149e3
Alex Mok <mokalexander00@gmail.com>
2019-12-02 18:43:57 -0500

    completed Part 1

--- 2018/day 7/Solution.js
+++ 2018/day 7/Solution.js
@@ -5,118 +5,67 @@ const array = fs
   .toString()
   .split('\n');
 array.length = array.length - 1;
-// console.log(array);
 
-const aList = Array.apply(null, new Array(26)).map(ele => []);
-const child = new Array(26).fill(false);
+const aList = {};
+const valid = new Array(26).fill(false);
 
-//First letter at index 5
-//Second letter at index 36
+//Part 1 - Find order that the steps must be completed in
+//Returns ACHOQRXSEKUGMYIWDZLNBFTJVP
+
 //A - char code 65
-//Z - char code 90
-//Return string should be 22 chars long
 function createGraph() {
   for (let i = 0; i < array.length; i++) {
+    //First letter at index 5
     let firstChar = array[i][5];
+
+    //Second letter at index 36
     let secChar = array[i][36];
 
-    aList[firstChar.charCodeAt() - 65].push(secChar);
-    child[secChar.charCodeAt() - 65] = true;
-  }
+    //If character exists in the puzzle we set to valid by normalizing to 0-25 index
+    valid[firstChar.charCodeAt() - 65] = true;
+    valid[secChar.charCodeAt() - 65] = true;
 
-  // console.log(aList, child);
+    if (!Array.isArray(aList[firstChar])) aList[firstChar] = [];
+
+    //Add the steps that must be completed first into that chars array
+    if (Array.isArray(aList[secChar])) aList[secChar].push(firstChar);
+    else aList[secChar] = [firstChar];
+  }
 }
 
 createGraph();
 
 function buildOrder() {
+  const keys = Object.keys(aList);
+
   let res = '';
-  let queue = [];
+  let pointer = 0;
 
-  for (let i = 0; i < child.length; i++) {
-    if (!child[i]) {
-      res += String.fromCharCode(i + 65);
-      queue.push(...aList[i]);
+  while (res.length !== keys.length) {
+    if (valid[pointer]) {
+      let currChar = String.fromCharCode(pointer + 65);
 
-      while (queue.length) {
-        queue.sort();
-        let curr = queue.shift();
-        let children = aList[curr.charCodeAt() - 65];
-        console.log(curr, queue);
-        res += curr;
-        if (children.length) queue.push(...children);
+      //If no other steps must be completed before the currChar
+      if (aList[currChar].length === 0) {
+        res += currChar;
+        valid[pointer] = false;
+
+        //Restart pointer from the beginning
+        pointer = -1;
+
+        //Remove the currChar from chars array that need it as a completed step
+        for (let i = 0; i < keys.length; i++) {
+          let key = keys[i];
+          if (aList[key].includes(currChar))
+            aList[key] = aList[key].filter(char => char !== currChar);
+        }
       }
     }
+
+    pointer++;
   }
 
-  console.log(aList, res, 'res');
+  console.log(res, 'res');
 }
 
-// buildOrder();
-
-const steps = input => {
-  let steps = array;
-
-  let finalOrder = [];
-  let allSteps = [];
-
-  // Prepare parent-child relationships
-  for (let i = 0; i < steps.length; i++) {
-    const step = steps[i];
-    const matchGroups = step.match(
-      /(\w+) (.) (\w+) (\w+) (\w+) (\w+) (\w+) (.)/
-    );
-
-    const parent = matchGroups[2];
-    const child = matchGroups[8];
-
-    if (!allSteps.find(x => x.step === parent)) {
-      allSteps.push({
-        step: parent,
-        children: [],
-        parents: [],
-      });
-    }
-
-    if (!allSteps.find(x => x.step === child)) {
-      allSteps.push({
-        step: child,
-        children: [],
-        parents: [],
-      });
-    }
-
-    const parentRelationship = allSteps.find(x => x.step === parent);
-
-    if (!parentRelationship.children.includes(child)) {
-      parentRelationship.children.push(child);
-    }
-
-    const childRelationship = allSteps.find(x => x.step === child);
-
-    if (!childRelationship.parents.includes(parent)) {
-      childRelationship.parents.push(parent);
-    }
-  }
-
-  while (allSteps.length) {
-    let potentialStep = allSteps
-      .filter(x => !x.parents.length)
-      .map(y => y.step)
-      .sort()[0];
-
-    finalOrder.push(potentialStep);
-    allSteps.splice(
-      allSteps.indexOf(allSteps.find(x => x.step === potentialStep)),
-      1
-    );
-
-    allSteps
-      .filter(a => a.parents.includes(potentialStep))
-      .forEach(y => y.parents.splice(y.parents.indexOf(potentialStep), 1));
-  }
-
-  console.log(finalOrder.join(''));
-};
-
-steps();
+buildOrder();
