0d9dd67a2999810ba05773094b03ce998e29fc94
Alex Mok <mokalexander00@gmail.com>
2019-12-02 17:53:36 -0500

    commented solution and added question

--- 2019/Day 2/Solution.js
+++ 2019/Day 2/Solution.js
@@ -3,43 +3,47 @@ const arr = fs
   .readFileSync('PuzzleInput.txt')
   .toString()
   .split(',')
-  .map(ele=>+ele)
+  .map(ele => +ele);
 
-
-// const array = [1,9,10,3,2,3,11,0,99,30,40,50]
+//Part 1 - Run input and return value in position 0
+//Returns 10566835
 function opCodes(noun, verb, array) {
-    array[1] = noun
-    array[2] = verb
+  array[1] = noun;
+  array[2] = verb;
 
-    let pointer = 0
-    let curr = array[pointer]
+  let pointer = 0;
+  let curr = array[pointer];
 
-    while(curr !== 99){
-        if(curr === 1){
-            let tmp = array[array[pointer + 1]] + array[array[pointer + 2]]
-            array[array[pointer + 3]] = tmp
-        } else if(curr === 2){
-            let tmp = array[array[pointer + 1]] * array[array[pointer + 2]]
-            array[array[pointer + 3]] = tmp
-        }
-
-        pointer += 4
-        curr = array[pointer]
+  while (curr !== 99) {
+    if (curr === 1) {
+      let tmp = array[array[pointer + 1]] + array[array[pointer + 2]];
+      array[array[pointer + 3]] = tmp;
+    } else if (curr === 2) {
+      let tmp = array[array[pointer + 1]] * array[array[pointer + 2]];
+      array[array[pointer + 3]] = tmp;
     }
 
-    return array[0]
+    pointer += 4;
+    curr = array[pointer];
+  }
+
+  return array[0];
 }
 
+// console.log(opCodes(12, 2, arr));
 
+//Part 2 - Find the values for 1st and 2nd position that will return 19690720
+//Returns 2347 (23, 47)
 function opCodes2() {
-    let noun = 0
-    let verb = 0
+  let noun = 0;
+  let verb = 0;
 
-    for(let noun = 0; noun < 100; noun++){
-        for(let verb = 0; verb < 100; verb++){
-            if(opCodes(noun,verb,[...arr]) === 19690720) console.log(100 * noun + verb)
-        }
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      if (opCodes(noun, verb, [...arr]) === 19690720)
+        console.log(100 * noun + verb);
     }
+  }
 }
 
-opCodes2()
+opCodes2();
