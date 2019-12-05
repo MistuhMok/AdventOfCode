ac001700a1e6e459ff51c22e08bfdef082ba79c8
MistuhMok <47574763+MistuhMok@users.noreply.github.com>
2019-12-04 23:50:17 -0500

    Added comments for part 1 and 2

--- 2019/Day 4/Solution.js
+++ 2019/Day 4/Solution.js
@@ -2,27 +2,31 @@
 const min = 248345
 const max = 746315
 
-//Part 1
+//Part 1 - Find how many different passwords fufill the critera
+//Comment out code on line 26 and 28 for Part 1 solution
 //Returns 1019
 
-//Part 2
+//Part 2 - Find how many different passwords fulfill the additional criteria
 //Returns - 660
 function count() {
     let res = 0
     for(let i = min; i <= max; i++){
-        let word = "" + i
+        let num = "" + i
         let valid = false
-        for(let j = 0; j < word.length; j++){
-            if(word[j] > word[j + 1]) {
+        for(let j = 0; j < num.length; j++){
+            if(num[j] > num[j + 1]) {
                 valid = false
                 break;
             }
 
-            if (word[j] === word[j - 1]) {
-                if (word[j + 1] !== word[j]) {
-                    if (word[j - 2] !== word[j]) {
+            if (num[j] === num[j - 1]) {
+                if (num[j + 1] !== num[j]) {
+
+                    //Part 2 logic - Checks if there is more than 2 of the same number
+                    if (num[j - 2] !== num[j]) {
                         valid = true;
                     }
+
                 }
             }
         }
