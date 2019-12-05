4837abd06c91003ed783a4a7fe763c28bf11f0c6
MistuhMok <47574763+MistuhMok@users.noreply.github.com>
2019-12-05 09:13:24 -0500

    refactor: remove logic to handle >2 params

--- 2019/Day 5/Solution.js
+++ 2019/Day 5/Solution.js
@@ -11,9 +11,13 @@ const arr = fs
 //Part 2 - Add 4 more instructions
 //Returns - 7079459
 
-// const arr = [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]
+// const arr = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+// 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+// 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
 function opCodes(array) {
+
     let pointer = 0
+
 
     while(pointer < array.length){
         let curr = '' + array[pointer]
@@ -26,45 +30,44 @@ function opCodes(array) {
         while(mode.length < 2) mode.push('0')
         
         if(opCode === '1'){
-            while(mode.length){
-                let currMode = mode.shift()
-    
-                if(currMode === '0') total += +array[+array[pointer]]
-                if(currMode === '1') total += +array[pointer]
-                
-                pointer++
-            }
+            let mode1 = mode[0]
+            let mode2 = mode[1]
+
+            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
+            let param2 = mode2 === '0' ? array[array[pointer + 1]] : array[pointer + 1]
+
+            total = param1 + param2
+
+            pointer += 2
         } else if(opCode === '2'){
-            total = 1
+            let mode1 = mode[0]
+            let mode2 = mode[1]
 
-            while(mode.length){
-                let currMode = mode.shift()
-    
-                if(currMode === '0') total *= array[array[pointer]]
-                if(currMode === '1') total *= +array[pointer]
+            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
+            let param2 = mode2 === '0' ? array[array[pointer + 1]] : array[pointer + 1]
 
-                pointer++
-            }
+            total = param1 * param2
+
+            pointer += 2
         } else if(opCode === '3'){
             //Part 1 - Input = 1
             //Part 2 - Input = 5
             let input = 5
-            array[+array[pointer]] = input
+            array[array[pointer]] = input
     
             pointer++
             continue;
         } else if(opCode === '4'){
-            
-            if(mode[0]==='1') console.log('OPCODE 04 OUTPUT', array[pointer])
+            if(mode[0] === '1') console.log('OPCODE 04 OUTPUT', array[pointer])
             else console.log('OPCODE 4 OUTPUT', array[+array[pointer]])
 
             pointer++
-
             continue;
         } else if(opCode === '5'){
             let mode1 = mode[0]
-            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
             let mode2 = mode[1]
+
+            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
             let param2 = mode2 === '0' ? array[array[pointer + 1]] : array[pointer + 1]
 
             if(param1 !== 0) pointer = param2
@@ -73,8 +76,9 @@ function opCodes(array) {
             continue;
         } else if(opCode === '6'){
             let mode1 = mode[0]
-            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
             let mode2 = mode[1]
+
+            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
             let param2 = mode2 === '0' ? array[array[pointer + 1]] : array[pointer + 1]
 
             if(param1 === 0) pointer = param2
@@ -83,8 +87,9 @@ function opCodes(array) {
             continue;
         } else if(opCode === '7'){
             let mode1 = mode[0]
-            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
             let mode2 = mode[1]
+
+            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
             let param2 = mode2 === '0' ? array[array[pointer + 1]] : array[pointer + 1]
 
             if(param1 < param2) total = 1
@@ -93,8 +98,9 @@ function opCodes(array) {
             pointer += 2
         } else if(opCode === '8'){
             let mode1 = mode[0]
-            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
             let mode2 = mode[1]
+
+            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
             let param2 = mode2 === '0' ? array[array[pointer + 1]] : array[pointer + 1]
 
             if(param1 === param2) total = 1
@@ -109,6 +115,7 @@ function opCodes(array) {
             return;
         }
 
+        //Modifies position if the opcode has a 3rd param for address
         if(pointer !== array.length) {
             let position = array[pointer]
             array[position] = total
