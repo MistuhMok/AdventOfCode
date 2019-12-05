94e2e34408b910b0bbcdff77dbe217c68a38185c
MistuhMok <47574763+MistuhMok@users.noreply.github.com>
2019-12-05 09:06:35 -0500

    feat: completed part 2
    I was initially under the incorrect assumption that the opcodes would potentially take in more than 2 arguments. I will refactor the code to account for this.

--- 2019/Day 5/Solution.js
+++ 2019/Day 5/Solution.js
@@ -8,11 +8,12 @@ const arr = fs
 //Part 1 - Add 2 new instructions and parameter modes
 //Returns - 9654885
 
+//Part 2 - Add 4 more instructions
+//Returns - 7079459
+
 // const arr = [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]
 function opCodes(array) {
-
     let pointer = 0
-
 
     while(pointer < array.length){
         let curr = '' + array[pointer]
@@ -46,7 +47,8 @@ function opCodes(array) {
             }
         } else if(opCode === '3'){
             //Part 1 - Input = 1
-            let input = 1
+            //Part 2 - Input = 5
+            let input = 5
             array[+array[pointer]] = input
     
             pointer++
@@ -59,6 +61,46 @@ function opCodes(array) {
             pointer++
 
             continue;
+        } else if(opCode === '5'){
+            let mode1 = mode[0]
+            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
+            let mode2 = mode[1]
+            let param2 = mode2 === '0' ? array[array[pointer + 1]] : array[pointer + 1]
+
+            if(param1 !== 0) pointer = param2
+            else pointer += 2
+
+            continue;
+        } else if(opCode === '6'){
+            let mode1 = mode[0]
+            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
+            let mode2 = mode[1]
+            let param2 = mode2 === '0' ? array[array[pointer + 1]] : array[pointer + 1]
+
+            if(param1 === 0) pointer = param2
+            else pointer += 2
+
+            continue;
+        } else if(opCode === '7'){
+            let mode1 = mode[0]
+            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
+            let mode2 = mode[1]
+            let param2 = mode2 === '0' ? array[array[pointer + 1]] : array[pointer + 1]
+
+            if(param1 < param2) total = 1
+            else total = 0
+
+            pointer += 2
+        } else if(opCode === '8'){
+            let mode1 = mode[0]
+            let param1 = mode1 === '0' ? array[array[pointer]] : array[pointer]
+            let mode2 = mode[1]
+            let param2 = mode2 === '0' ? array[array[pointer + 1]] : array[pointer + 1]
+
+            if(param1 === param2) total = 1
+            else total = 0
+
+            pointer += 2
         } else if(curr.slice(-2) === '99'){
             console.log('OPCODE 99')
             return
