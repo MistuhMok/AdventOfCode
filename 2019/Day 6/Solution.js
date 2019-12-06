211a06d28331864e1c697cca084d97689138d10c
MistuhMok <47574763+MistuhMok@users.noreply.github.com>
2019-12-06 02:50:08 -0500

    feat: complete part 2

--- 2019/Day 6/Solution.js
+++ 2019/Day 6/Solution.js
@@ -41,3 +41,35 @@ function countOrbits() {
 }
 
 countOrbits()
+
+//Part 2 - Calculate the distance from YOU to SAN
+//Returns - 304
+
+function findOrbitalTransfers(){
+    //Create a hash of the path leading up to 'YOU' and how far away each planet is
+    const youPath = {'YOU': 0}
+    let curr = 'YOU'
+    let next = parents[curr]
+    let counter = 0
+    while(next){
+        youPath[next] = ++counter
+        curr = next
+        next = parents[curr]
+    }
+
+    //Find a path from 'SAN' back to root until one of the planets is on the path leading up to 'YOU'
+    curr = 'SAN'
+    counter = 0
+    next = parents[curr]
+
+    while(youPath[next] === undefined){
+        counter++
+        curr = next
+        next = parents[curr]
+    }
+
+    //The counter will double count the planet they meet up on
+    console.log(youPath[next] + counter - 1)
+}
+
+findOrbitalTransfers()
