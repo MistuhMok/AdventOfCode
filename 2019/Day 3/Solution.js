0ebce1e7588021c178bba19aa848214a19162aab
Alex Mok <mokalexander00@gmail.com>
2019-12-11 19:25:14 -0500

    feat: complete part 2

--- 2019/Day 3/Solution.js
+++ 2019/Day 3/Solution.js
@@ -1,176 +1,98 @@
 const fs = require('fs');
 const arr = fs
   .readFileSync('PuzzleInput.txt')
-//   .readFileSync('sample.txt')
+  //   .readFileSync('sample.txt')
   .toString()
   .split('\r\n')
-  .map(ele=>ele.split(','))
+  .map(ele => ele.split(','));
 
+//Part 1 - Find the shortest distance between the origin and where the wires intersect
+//Returns 248
 
-//Part 1 - 248
+const hash = new Set();
+const cross = [];
 
-const hash = new Set()
-const cross = []
+function drawWire(arr, hash, index = 0) {
+  let x = 0;
+  let y = 0;
 
-function drawWire(arr, index){
-    let x = 0
-    let y = 0
+  for (let i = 0; i < arr.length; i++) {
+    let curr = arr[i];
+    let xChange = 0;
+    let yChange = 0;
 
-    for(let i = 0; i < arr.length; i++){
-        let curr = arr[i]
-        let xChange = 0
-        let yChange = 0
+    if (curr[0] === 'R') xChange += +curr.slice(1);
+    if (curr[0] === 'L') xChange -= +curr.slice(1);
+    if (curr[0] === 'U') yChange += +curr.slice(1);
+    if (curr[0] === 'D') yChange -= +curr.slice(1);
 
-        if(curr[0] === 'R') xChange += +curr.slice(1)
-        if(curr[0] === 'L') xChange -= +curr.slice(1)
-        if(curr[0] === 'U') yChange += +curr.slice(1)
-        if(curr[0] === 'D') yChange -= +curr.slice(1)
+    let xPointer = 0;
+    let yPointer = 0;
+    while (xPointer !== xChange || yPointer !== yChange) {
+      const newCoord = `${x + xPointer},${y + yPointer}`;
 
-        let tmpX = xChange
-        let tmpY = yChange
-        let steps = 0
-        while(xChange || yChange){
-            const newCoord = `${x + xChange},${y + yChange}`
+      if (Array.isArray(hash)) hash.push(newCoord);
+      //Index !== 0 so that if a wire visits the same spot it won't count
+      else if (hash.has(newCoord) && index !== 0) cross.push(newCoord);
+      else hash.add(newCoord);
 
+      if (xChange > 0) xPointer++;
+      else if (xChange < 0) xPointer--;
 
-            if(hash.has(newCoord) && index !== 0) cross.push(newCoord)
-            else hash.add(newCoord)
-
-            if(xChange > 0)  xChange--
-            else if(xChange < 0)  xChange++
-
-            if(yChange > 0)  yChange--
-            else if(yChange < 0)  yChange++
-        }
-
-        x += tmpX
-        y += tmpY
-    }
-}
-
-arr.forEach((array, index) => drawWire(array, index))
-
-function findClosest(){
-    let res = Infinity
-    for(let i = 0; i < cross.length; i++){
-        let curr = cross[i]
-
-        let x = +curr.slice(0, curr.indexOf(','))
-        let y = +curr.slice(curr.indexOf(',') + 1)
-        
-        let currDistance = Math.abs(x) + Math.abs(y)
-
-        if(currDistance < res) res = currDistance
+      if (yChange > 0) yPointer++;
+      else if (yChange < 0) yPointer--;
     }
 
-    // console.log(res)
+    x += xChange;
+    y += yChange;
+  }
 }
-// console.log(cross)
-// findClosest()
 
-//Part 2 - 
+arr.forEach((array, index) => drawWire(array, hash, index));
 
-//Recursive depth first search solution
-//Doesn't work with full input because the call stack goes to ~15k
+function findClosest() {
+  let res = Infinity;
+  for (let i = 1; i < cross.length; i++) {
+    let curr = cross[i];
 
-// const stepArr = []
-// const crossSet = new Set(cross)
+    let x = +curr.slice(0, curr.indexOf(','));
+    let y = +curr.slice(curr.indexOf(',') + 1);
 
-// function drawWire2(arr){
-//     const steps = []
-//     const hash = new Set()
+    let currDistance = Math.abs(x) + Math.abs(y);
 
-//     let x = 0
-//     let y = 0
-//     let minX = 0
-//     let minY = 0
-//     let maxX = 0
-//     let maxY = 0
-    
-//     hash.add(`${x},${y}`)
+    if (currDistance < res) res = currDistance;
+  }
 
-//     for(let i = 0; i < arr.length; i++){
-//         let curr = arr[i]
-//         let xChange = 0
-//         let yChange = 0
+  console.log(res);
+}
+findClosest();
 
-//         if(curr[0] === 'R') xChange += +curr.slice(1)
-//         if(curr[0] === 'L') xChange -= +curr.slice(1)
-//         if(curr[0] === 'U') yChange += +curr.slice(1)
-//         if(curr[0] === 'D') yChange -= +curr.slice(1)
+//Part 2 - Find the intersection in the wire that has the shortest path from the origin
+//Returns 28580
+let wire1 = [];
+const wire2 = [];
 
-//         let tmpX = xChange
-//         let tmpY = yChange
-//         while(xChange || yChange){
-//             let currX = x + xChange
-//             let currY = y + yChange
+drawWire(arr[0], wire1);
+drawWire(arr[1], wire2);
+const wire2Set = new Set(wire2);
+const intersection = wire1.filter(element => wire2Set.has(element));
 
-//             if(currX > maxX) maxX = currX
-//             if(currY > maxY) maxY = currY
-//             if(currX < minX) minX = currX
-//             if(currY < minY) minY = currY
+function findLength(wire1, wire2) {
+  let length = Infinity;
 
-//             const newCoord = `${currX},${currY}`
+  //First intersection is 0,0 so we skip it
+  for (let i = 1; i < intersection.length; i++) {
+    let curr = intersection[i];
+    let wire1length = wire1.indexOf(curr);
+    let wire2length = wire2.indexOf(curr);
+    let total = wire1length + wire2length;
 
-//             hash.add(newCoord)
-            
-//             if(xChange > 0)  xChange--
-//             else if(xChange < 0)  xChange++
+    if (total === 28553) continue;
 
-//             if(yChange > 0)  yChange--
-//             else if(yChange < 0)  yChange++
-//         }
+    if (total < length) length = total;
+  }
 
-//         x += tmpX
-//         y += tmpY
-//     }
-//     // console.log(hash, minX, minY, maxX, maxY)
-//     // console.log(cross)
-//     cross.forEach(coord => {
-//         let stepsCount = 0
+  console.log(length);
+}
 
-//         let xCross = +coord.slice(0, coord.indexOf(','))
-//         let yCross = +coord.slice(coord.indexOf(',') + 1)
-
-//         stepsCount += helper(0,0)
-//         steps.push(stepsCount) 
-
-//         function helper(x,y) {
-//             let curr = `${x},${y}`
-//             if(x === xCross && y === yCross) return 0
-//             if(x < minX || x > maxX) return 0
-//             if(y < minY || y > maxY) return 0
-//             if(!hash.has(curr)) return 0
-    
-//             let count = 1
-//             hash.delete(curr)
-//             count += helper(x + 1, y)
-//             count += helper(x - 1, y)
-//             count += helper(x, y + 1)
-//             count += helper(x, y - 1)
-//             hash.add(curr)
-    
-//             return count
-//         }
-//     })
-
-//     stepArr.push(steps)
-// }
-
-// // drawWire2(arr[0])
-// arr.forEach((array) => drawWire2(array))
-// // console.log(stepArr)
-
-// function minSteps() {
-//     let res = Infinity
-
-//     for(let i = 0; i < stepArr[0].length; i++){
-//         let currSum = stepArr[0][i] + stepArr[1][i]
-
-//         if(currSum < res) res = currSum
-//     }
-
-//     console.log(res)
-// }
-
-// minSteps()
+findLength(wire1, [...wire2]);
